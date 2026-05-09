0774468803ad65f268b73963e39e1a730f4020ff
amine <amine.kazzout@mobiblanc.com>
2026-05-09 22:05:16 +0100

    add github action

--- src/test/java/com/demoQaAutomation/utils/DriverManager.java
+++ src/test/java/com/demoQaAutomation/utils/DriverManager.java
@@ -3,6 +3,7 @@ package com.demoQaAutomation.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager
 {
@@ -11,7 +12,14 @@ public class DriverManager
     public static WebDriver getDriver() {
         if(driver==null){
             WebDriverManager.chromedriver().setup();
-            driver=new ChromeDriver();
+            ChromeOptions options=new ChromeOptions();
+            if("true".equals(System.getProperty("headless"))){
+                options.addArguments("--headless");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+            }
+
+            driver=new ChromeDriver(options);
             driver.manage().window().maximize();
         }
         return driver;
